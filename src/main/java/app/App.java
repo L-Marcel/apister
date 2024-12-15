551b8a08d05c443faa3e14bb70849c84721ae39e
l-marcel <lmgh1312@gmail.com>
2024-12-14 21:07:52 -0300

    Adicionando um pouco de estilização.

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -4,9 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import app.log.Log;
 import app.secure.ShutdownHook;
@@ -24,6 +27,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        loadFonts();
         scene = new Scene(load("projects"), 640, 480);
         applyCss("projects");
         stage.setMinHeight(400);
@@ -41,16 +45,16 @@ public class App extends Application {
     private static void applyCss(String page) {
         scene.getStylesheets().clear();
         scene.getStylesheets().addAll(
-            App.class.getResource("global.css").toString(),
-            App.class.getResource(page + ".css").toString()
+            App.class.getResource("global.css").toExternalForm(),
+            App.class.getResource(page + ".css").toExternalForm()
         );
     };
 
     public static void applyCss(Parent parent, String page) {
         parent.getStylesheets().clear();
         parent.getStylesheets().addAll(
-            App.class.getResource("global.css").toString(),
-            App.class.getResource(page + ".css").toString()
+            App.class.getResource("global.css").toExternalForm(),
+            App.class.getResource(page + ".css").toExternalForm()
         );
     };
 
@@ -63,5 +67,33 @@ public class App extends Application {
     public static Parent load(String page) throws IOException {
         FXMLLoader loader = new FXMLLoader(App.class.getResource(page + ".fxml"));
         return loader.load();
+    };
+
+    private static void loadFonts() {
+        String[] fonts = {
+            "Oswald/Oswald-Bold.ttf",
+            "Oswald/Oswald-ExtraLight.ttf",
+            "Oswald/Oswald-Light.ttf",
+            "Oswald/Oswald-Medium.ttf",
+            "Oswald/Oswald-Regular.ttf",
+            "Oswald/Oswald-SemiBold.ttf",
+            // "Oswald/Oswald.ttf",
+            "Quattrocento/Quattrocento-Bold.ttf",
+            "Quattrocento/Quattrocento-Regular.ttf"
+        };
+
+        for (String font : fonts) {
+            try {
+                Font.loadFont(
+                    Files.newInputStream(
+                        Path.of(
+                            App.class.getResource("fonts/" + font).toURI()
+                        )
+                    ),
+                13);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }; 
+        };
     };
 };
--- src/main/java/app/layout/ProjectCell.java
+++ src/main/java/app/layout/ProjectCell.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import app.App;
 import app.controllers.ProjectCellController;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.control.ListCell;
 
@@ -11,6 +12,13 @@ public class ProjectCell extends ListCell<String> {
     @Override
     protected void updateItem(String project, boolean empty) {
         super.updateItem(project, empty);
+
+        setPadding(new Insets(0));
+        getStyleClass().add("project-cell");
+        if(getIndex() % 2 == 1) {
+            getStyleClass().add("project-cell-alt");
+        };
+
         if (empty || project == null) {
             setText(null);
             setGraphic(null);
